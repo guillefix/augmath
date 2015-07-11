c9495820d42b4978f6eab334531ad08a162980c4
Guillermo Valle <guillefix@gmail.com>
2015-07-12 00:24:23 +0200

    Added more manipulations and done some changes

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 //Need to maybe add more validation of the operations you are doing. In any case, this is such a big project, there'll always be work to do.
 
-var math_str, h_eq_shift=0, v_eq_shift=0, equals_position = {left: 0, top: 0}, manip = "term", multi_select = 0;
+var math_str, 
+	h_eq_shift=0, 
+	v_eq_shift=0, 
+	equals_position = {left: 0, top: 0}, 
+	manip = "term", 
+	multi_select = false, 
+	step_duration = 700;
 
 //UI stuff
-$("#term").on("click", function () {manip = "term";})
-$("#factor").on("click", function () {manip = "factor";})
-$("#multi_select").on("click", function () {multi_select = document.getElementById("multi_select").checked;})
+$("#term").on("click", function () {manip = "term";});
+$("#factor").on("click", function () {manip = "factor";});
+$("#multi_select").on("click", function () {multi_select = document.getElementById("multi_select").checked;});
 
+
+//MANIPULATIVES & RENDERING//
 
 //A function to render the math and setup the event handlers
 function prepare(math) {
@@ -26,51 +34,54 @@ if ($objects.first().text() === "+" || $objects.first().text() === "−") {$oper
 
 
 $objects.on("click", function() {
-	//select a TERM. this works with only simple parenthesis expressions!!
+	//select a TERM. this works with only simple parenthesis expressions!! although im improving it, im still not sure it works in all generality
 	if (manip === "term") {
 		//select whole TERM, both symbols before and after
 		$this = $(this);
-		if ($this.prevAll(".mopen").length - $this.prevAll(".mclose").length === 0 ) {
-			$prev = $this.prevUntil(".mbin, .mrel");
-		} else {
-			$prev = $this.prevUntil($this.prevAll(".mopen").first().prev());
-		}
+		//if ($this.prevAll(".mopen").length - $this.prevAll(".mclose").length === 0 ) {
+			$prev = $this.prevUntil(".mbin, .mrel, .mopen");
+			topkek = 0;
+			while ($prev.first().prevAll(".mopen").length - $prev.first().prevAll(".mclose").length !== 0 && topkek<10) {
+				$prev = $prev.add($prev.first().prev());
+				$prev = $prev.add($prev.first().prevUntil(".mbin, .mrel"));
+				topkek++;
+			}
 
 		if ($prev.length == 0 && $this.prev(".mrel").length == 0) {
 			$prev = $prev.add($this.prev());
 		} else if ($prev.prev(".mrel").length == 0) {
-			$prev = $prev.add($prev.last().prev());
+			$prev = $prev.add($prev.first().prev());
 		}
 
-		if ($this.nextAll(".mopen").length - $this.nextAll(".mclose").length === 0) {
-			$next = $this.nextUntil(".mbin, .mrel");;
-		} else {
-			$next = $this.nextUntil($this.nextAll(".mclose").last().next());
-		}
+		//if ($this.nextAll(".mopen").length - $this.nextAll(".mclose").length === 0) {
+			$next = $this.nextUntil(".mbin, .mrel, .mclose");
+			while ($next.last().nextAll(".mopen").length - $next.last().nextAll(".mclose").length !== 0) {
+				console.log("lol2");
+				$next = $next.add($next.last().next());
+				$next = $next.add($next.last().nextUntil(".mbin, .mrel"));
+			}
 		$this = $this.add($prev);
 		$this = $this.add($next);
-		$this.toggleClass("selected");
-		//if (multi_select === 0) {$this.siblings(".selected").not($this).toggleClass("selected");} need to fix multiselect
-		$manipulatives.find(".selected").not($this).toggleClass("selected");
-		$manipulatives.siblings(".selected").not($this).toggleClass("selected");
+		
 	}
 
 	//select a FACTOR
 	else if (manip === "factor") {
 		$this = $(this);
 		if ($this.filter(".mopen, .mclose").length === 0) {
-			$this.toggleClass("selected");
-			//if (multi_select === 0) {$this.siblings(".selected").not($this).toggleClass("selected");} need to fix multiselect
-			$manipulatives.find(".selected").not($this).toggleClass("selected");
-			$manipulatives.siblings(".selected").not($this).toggleClass("selected");
+			$this = $this.not(".minner");
 		} else {
 			do {
 				$this = $this.add($this.next()); 
 			} while (!($this.filter(".mopen").length-$this.filter(".mclose").length === 0));
-			$this.toggleClass("selected");
-			$this.siblings(".selected").not($this).toggleClass("selected");
 		}
 	}
+	$this.toggleClass("selected");
+	if (!multi_select)	{
+			$manipulatives.find(".selected").not($this).toggleClass("selected");
+			$manipulatives.siblings(".selected").not($this).toggleClass("selected");
+	}
+
 });
 
 //More compatible TERM selector, works by clicking on its operator on the left
@@ -83,6 +94,7 @@ $operators.on("click", function() {
 	$this.siblings(".selected").not($this).toggleClass("selected");
 });
 
+	//repositioning equals so that it's always in the same place
 	new_equals_position = $manipulatives.siblings(".mrel").offset();
 	if (equals_position.left !== 0) {h_eq_shift += equals_position.left-new_equals_position.left;}
 	if (equals_position.top !== 0) {v_eq_shift += equals_position.top-new_equals_position.top;}
@@ -103,7 +115,9 @@ var initial_math_str = "2x+1=3";
 
 $(document).ready(prepare(initial_math_str));
 
-//prepare term for new expression
+//MANIPULATIONS//
+
+//prepare term for new expression by finding it in the math_str
 function prepare_term() {
 	term_num = $selected.first().prevAll(".mbin, .mrel").length; //no +1 as the equal sign isn't part of term
 	signs = math_str.match(/[+=-]+/g);
@@ -143,7 +157,7 @@ function prepare_term() {
 	}
 }
 
-//change a term of side
+//change a term of side. things to fix about this when multi_select is on. also fix animation when on RHS the term doesnt have a sign
 change_side = document.getElementById("change_side");
 change_side.onclick = function() {
 	$selected = $(".selected").not(".sqrt span, .minner span");
@@ -156,8 +170,8 @@ change_side.onclick = function() {
 	//different behaviour depending on which side of the eq., determined by existence of equal sign before or after
 	if ($selected.prevAll(".mrel").length === 0) { //before eq sign
 		offset = (end_of_equation.left-selected_position.left)+width_last_term;
-		$selected.first().prevAll().animate({left:selected_width}, 1000);
-		$selected.animate({left:offset}, 1000).promise().done(function() {
+		$selected.first().prevAll().animate({left:selected_width}, step_duration);
+		$selected.animate({left:offset}, step_duration).promise().done(function() {
 			$selected = $(".selected").add($(".selected").find("*"));
 			prepare_term();
 			math_str = math_str.replace(selected_term, "")+new_term;
@@ -166,9 +180,9 @@ change_side.onclick = function() {
 
 	} else { //after eq sign
 		offset = (equals_position.left-selected_position.left)-selected_width;
-		$selected.prevAll(".mrel").first().prevAll().animate({left:-selected_width}, 1000);
-		$selected.last().nextAll().animate({left:-selected_width}, 1000);
-		$selected.animate({left:offset}, 1000).promise().done(function() {
+		$selected.prevAll(".mrel").first().prevAll().animate({left:-selected_width}, step_duration);
+		$selected.last().nextAll().animate({left:-selected_width}, step_duration);
+		$selected.animate({left:offset}, step_duration).promise().done(function() {
 			$selected = $(".selected").add($(".selected").find("*"));
 			prepare_term();
 			math_str = math_str.replace(selected_term, "");
@@ -178,14 +192,19 @@ change_side.onclick = function() {
 	}
 };
 
+//prepare factor
 function prepare_factor() {
-	selected_term = $(".selected").text().replace("−", "-"); //works except with fractions and parenthesis
+	selected_term = $(".selected").text().replace("−", "-");
+	if ($(".selected").text().indexOf("√") > -1) {
+		selected_term = selected_term.replace("√", "\\sqrt{") + "}";
+	}
+	selected_term = selected_term.replace(/[^\x00-\x7F]/g, ""); //removing non-ASCII characters that crop up from the text() method
 }
 
 //divide by factor
 divide_factor = document.getElementById("divide_factor");
 divide_factor.onclick = function() {
-	$selected = $(".selected").not(".sqrt span")
+	$selected = $(".selected").not(".sqrt span");
 	end_of_equation = $manipulatives.last().offset();
 	beginning_of_equation = $manipulatives.first().offset();
 	$equals = $manipulatives.siblings(".mrel");
@@ -193,8 +212,8 @@ divide_factor.onclick = function() {
 	RHS_width = tot_width($manipulatives.siblings(".mrel").nextAll(), false);
 	h_offset = $equals.offset().left - $selected.offset().left + tot_width($equals, true) + (RHS_width)/2;
 	v_offset = $selected.outerHeight(includeMargin=true)/2;
-	$equals.nextAll().animate({top:-v_offset, left:tot_width($selected, true)/2}, 1000);
-	$selected.animate({left:h_offset, top:v_offset}, 1000).promise().done(function() {
+	$equals.nextAll().animate({top:-v_offset, left:tot_width($selected, true)/2}, step_duration);
+	$selected.animate({left:h_offset, top:v_offset}, step_duration).promise().done(function() {
 		prepare_factor();
 		math_str = math_str.replace(selected_term, "");
 		math_HS = math_str.split("=");
@@ -204,3 +223,34 @@ divide_factor.onclick = function() {
 
 }
 
+//prepare expression
+function prepare_expression() {
+	selected_term = $(".selected").text().replace("−", "-");
+	selected_term_str = selected_term;
+	if ($(".selected").text().indexOf("√") > -1) {
+		selected_term = selected_term.replace("√", "") + "^0.5";
+		selected_term_str = selected_term;
+	} else if ($(".selected").filter(".minner").length !== 0) {
+		numerator = $(".selected").find(".mord").last().text();
+		denominator = $(".selected").find(".mord").first().text();
+		selected_term_str = numerator + "/" + denominator;
+		selected_term = "\\frac{" + numerator + "}{" + denominator + "}";
+	}
+	selected_term = selected_term.replace(/[^\x00-\x7F]/g, ""); //removing non-ASCII characters that crop up from the text() method
+
+	new_term = math.eval(selected_term_str).toString();
+
+
+}
+
+//evaulate simple sum or multiplication
+eval = document.getElementById("eval");
+eval.onclick = function() {
+	equals_position = $manipulatives.siblings(".mrel").offset(); //necessary for keeping equals in the same position when prepare is called
+	$selected = $(".selected")//.not(".sqrt span, .minner span");
+	$selected.animate({"font-size": 0, opacity: 0}, step_duration).css('overflow', 'visible').promise().done(function() {
+		prepare_expression();
+		math_str = math_str.replace(selected_term, new_term);
+		prepare(math_str);
+	});
+}
